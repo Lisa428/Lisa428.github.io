2e90300b4d1da5ef2d15b4e4b9d1a2ebc0a07b19
Lisa Helminger <lisa-maria.helminger@uibk.ac.at>
2018-05-02 16:55:42 +0200

    Wetterstationen mit T auf map

--- awstirol_all.js
+++ awstirol_all.js
@@ -1,7 +1,7 @@
 
 let myMap = L.map('mapdiv'); //http://leafletjs.com/reference-1.3.0.html#map-l-map
 //let myLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'); //erstellt neue Variable myLayer, aus Leafleat Bibliothek Layer; z=zoomlevel 
-
+const awsGroup = L.featureGroup();  //neue featureGroup erstellt
 let myLayers = {
     
     osm : L.tileLayer( //http://leafletjs.com/reference-0.7.7.html#tilelayer
@@ -57,6 +57,7 @@ let myMapControl = L.control.layers({   //http://leafletjs.com/reference-0.7.7.h
     "basemap.at Orthofoto" : myLayers.bmaporthofoto30cm,
 },{
     "basemap.at Overlay" : myLayers.bmapoverlay,
+    'Wetterstationen: ': awsGroup,   //feature Group dem Overlay-Layer hinzufügen
 },{
     collapsed: false //http://leafletjs.com/reference-0.7.7.html#control-layers-collapsed
     
@@ -64,8 +65,24 @@ let myMapControl = L.control.layers({   //http://leafletjs.com/reference-0.7.7.h
 myMap.addControl(myMapControl);  //http://leafletjs.com/reference-0.7.7.html#map-addcontrol
 
 
+
 L.control.scale( { //http://leafletjs.com/reference-0.7.7.html#control-scale
     position: 'bottomleft', // (wäre eigentlich default) http://leafletjs.com/reference-0.7.7.html#control-scale-position
     maxWidth:200, //http://leafletjs.com/reference-0.7.7.html#control-scale-maxwidth
     imperial : false} //http://leafletjs.com/reference-0.7.7.html#control-scale-imperial
 ).addTo(myMap);
+
+
+console.log('stationen: ', stationen);
+
+myMap.addLayer(awsGroup);
+
+let geojson=L.geoJSON(stationen).addTo(myMap);
+geojson.bindPopup(function(layer) {
+    const props = layer.feature.properties;
+    const popupText= `<h1>${props.name}</h1>
+    <p>Temperatur: ${props.LT} °C</p>`;  //zwischen ${} ist Variable, sonst Text
+    return popupText;
+});
+
+myMap.fitBounds(geojson.getBounds()); //regelt zoomstufe des layer
